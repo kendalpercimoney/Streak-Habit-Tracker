9331f3bfcba728e14a2e6512e526dd34542cd58e
Kendal Percimoney <48430773+kendalpercimoney@users.noreply.github.com>
2024-07-30 09:01:58 +0100

    Initial Commit

---
+++ Streak! Habit Tracker/Streak__Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Streak__Habit_TrackerApp.swift
+//  Streak! Habit Tracker
+//
+//  Created by Kendal Percimoney on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Streak__Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
